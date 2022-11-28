b1cb5ea9f2aca6f89491cf22537856347c7936a9
Saiful Islam Sarfaraz <saifulislamsarfaraz@gmail.com>
2022-11-28 13:20:27 +0600

    Add files via upload

---
+++ CURD/code.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+require 'dbcon.php';
+if(isset($_POST['save_student']))
+{
+    $name = mysqli_real_escape_string($con, $_POST['name']);
+    $email = mysqli_real_escape_string($con, $_POST['email']);
+    $phone = mysqli_real_escape_string($con, $_POST['phone']);
+    $course = mysqli_real_escape_string($con, $_POST['course']);
+
+    $query = "INSERT INTO students (name,email,phone,course) VALUES ('$name','$email','$phone','$course')";
+
+    $query_run = mysqli_query($con, $query);
+    if($query_run)
+    {
+        $_SESSION['message'] = "Student Created Successfully";
+        header("Location: student-create.php");
+        exit(0);
+    }
+    else
+    {
+        $_SESSION['message'] = "Student Not Created";
+        header("Location: student-create.php");
+        exit(0);
+    }
+}
+?>
